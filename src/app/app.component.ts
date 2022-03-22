2eb18e5ebc26d9daf36a496d27661d99c5d54b50
tikaoo <patrick.cavalcante.gomes@gmail.com>
2022-03-21 21:24:12 -0300

    entregando tarefa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+ 
 export class AppComponent {
-  title = 'app';
+ 
+  name:string='';
+  sobrenome:string ='';
+  email:string='';
+  userName:string='';
+  cpf:string='';
+  celular:string='';
+  endereco:string='';
+  complemento:string='';
+  senha1:string='';
+  senha2:string='';
+  telefone:any []=[];
+
+constructor( private snack : MatSnackBar){}
+
+cel(){
+  this.telefone.push([this.telefone])
+}
+
+submit():void{
+if(this.senha1===this.senha2 || this.senha1 ==='' || this.senha2 ==='' ){
+  this.snack.open('Senhas diferente,verifique!', 'Fechar')
+}else {
+  this.snack.open('Cadastrado realizado com Sucesso!', 'Fechar')
+}
+
+}
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
